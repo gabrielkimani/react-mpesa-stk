0cb79f1cd8546073f2de893ae275097c4dbdc356
gabby429 <65331891+gabrielkimani@users.noreply.github.com>
2022-04-28 12:45:47 +0300

    initial

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from 'react'
 import styles from './styles.module.css'
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return (
+    <div className={styles.mpesa_wrapper}>
+      <div className={styles.inner_wrapper}>
+        <span className='title'>Payment details :Gcash Activation fee</span>
+        <span className={styles.amount}>Amount: KES 1,000</span>
+        <span className={styles.number}>Mpesa number:254722222222</span>
+        <span className={styles.button}>
+          <button className={styles.button_wrapper}>
+            <span className={styles.button_text}>Pay</span>
+          </button>
+        </span>
+      </div>
+    </div>
+  )
 }
